dfe2d5881d5857c50ce9bbbdcf34dc16f7f6aa5e
djan2003 <67077309+djan2003@users.noreply.github.com>
2021-02-25 08:53:14 +0300

    step11 complited(10/10)

--- mainTodolis/src/state/tasks-reducer.ts
+++ mainTodolis/src/state/tasks-reducer.ts
@@ -60,24 +60,32 @@ export const tasksReducer = (state: TasksStateType = initialState, action: Actio
         }
         case 'CHANGE-TASK-STATUS': {
             let todolistTasks = state[action.todolistId];
-            // найдём нужную таску:
+            state[action.todolistId]=todolistTasks.map(t=>
+                t.id === action.taskId
+                    ?{...t,isDone:action.isDone}
+                    :t)
+            /*// найдём нужную таску:
             let task = todolistTasks.find(t => t.id === action.taskId);
             //изменим таску, если она нашлась
             if (task) {
                 task.isDone = action.isDone;
             }
-            state[action.todolistId]=[...todolistTasks]
+            state[action.todolistId]=[...todolistTasks]*/
             return ({...state});
         }
         case 'CHANGE-TASK-TITLE': {
             let todolistTasks = state[action.todolistId];
-            // найдём нужную таску:
+            state[action.todolistId]=todolistTasks.map(t=>
+                t.id === action.taskId
+                    ?{...t,title:action.title}
+                    :t)
+           /* // найдём нужную таску:
             let task = todolistTasks.find(t => t.id === action.taskId);
             //изменим таску, если она нашлась
             if (task) {
                 task.title = action.title;
             }
-            state[action.todolistId]=[...todolistTasks]
+            state[action.todolistId]=[...todolistTasks]*/
             return ({...state});
         }
         case 'ADD-TODOLIST': {
